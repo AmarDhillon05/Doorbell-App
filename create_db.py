3caabc0c7003a88d99f9bed4aa906ee3070c1147
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2024-08-05 11:24:45 -0400

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,9 +1,10 @@
 import mysql.connector
+import os
 
 camdb = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    passwd = 'Rb878425!',
+    passwd = os.environ.get("PSWD"),
     database = 'camdb'
 )
 cursor = camdb.cursor()
